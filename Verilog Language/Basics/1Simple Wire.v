e7cc36a8aceaa16aa924a951397eefc7ab29f2b8
TOASTED DUCK <kasmykas2233@gmail.com>
2025-06-14 15:36:41 +0800

    Delete Verilog Language/Basics (previous) directory

--- Verilog Language/Basics/1Simple Wire.v
+++
@@ -1,6 +0,0 @@
-module top_module( input in, output out );
-
-    //connecting wire output to wire input 
-    assign out = in;
-    
-endmodule
--- Verilog Language/Basics/4AND Gate.v
+++
@@ -1,9 +0,0 @@
-module top_module( 
-    input a, 
-    input b, 
-    output out );
-
-    // '&' work as a bitewise AND gate
-    assign out = a & b;
-    
-endmodule
