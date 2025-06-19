054c1423ba9d50cc9848b0a446b410c2e989664d
TOASTED DUCK <kasmykas2233@gmail.com>
2025-06-19 20:10:51 +0800

    Delete Getting Started (previous) directory

--- Getting Started/Getting Started.v
+++
@@ -1,6 +0,0 @@
-module top_module( output one );
-
-    //this assign wire "one" as 1, infinitly
-    assign one = 1;
-
-endmodule
--- Getting Started/Output Zero.v
+++
@@ -1,4 +0,0 @@
-module top_module(output zero);
-    //this module does not need any code
-    //wire is automatically set to 0 in Verilog
-endmodule
